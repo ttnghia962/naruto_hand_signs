9efd0de036a9cb827b9ff9dfea54f86081f07a40
ttnghia962 <ttnghia962@gmail.com>
2024-02-05 22:18:15 +0700

    1st

---
+++ Readme.md
@@ -0,0 +1,4 @@
+### 1. Connect python with google drive
+* use any ways to connect
+### 2. Processing data 
+* duplicated img for 4 times. (rotate, flip, distored)
---
+++ connect_ggdrive.py
@@ -0,0 +1,9 @@
+'''
+    Python 3.10.7 or greater
+    The pip package management tool
+    A Google Cloud project.
+    A Google account with Google Drive enabled.
+ ### Install python library:
+    pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+
+'''
